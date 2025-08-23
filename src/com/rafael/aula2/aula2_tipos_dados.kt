9d2fd7cbc22091356879b9ffcd9600df16b876cc
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-08-23 11:34:48 -0300

    feat: aula 2 tipos de dados numéricos - flutuantes

--- src/com/rafael/aula2/aula2_tipos_dados.kt
+++ src/com/rafael/aula2/aula2_tipos_dados.kt
@@ -2,9 +2,13 @@ package com.rafael.aula2
 
 fun main() {
 
-    //numéricos
+    // Numéricos - Inteiro
     val byte: Byte = 10
     val short: Short = 32_767
     val int: Int = 30_000_000
     val long: Long = 1234578965431
+
+    // Numéricos - Flutuantes
+    val float: Float = 32f
+    var double: Double = 3.14
 }
