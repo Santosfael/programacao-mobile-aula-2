3488e588d6192adf93ff133201cde27da3b71b1b
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-08-23 11:22:36 -0300

    feat: aula 2 tipos de dados numéricos - inteiros

---
+++ src/com/rafael/aula2/aula2_tipos_dados.kt
@@ -0,0 +1,10 @@
+package com.rafael.aula2
+
+fun main() {
+
+    //numéricos
+    val byte: Byte = 10
+    val short: Short = 32_767
+    val int: Int = 30_000_000
+    val long: Long = 1234578965431
+}
