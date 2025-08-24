c09e9f511c35dbadef73d01eec27b458ba9ce3df
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-08-23 23:19:55 -0300

    feat: aula 2 controle de fluxo - when usando intervalos in

--- src/com/rafael/aula2/aula2_controle_fluxo.kt
+++ src/com/rafael/aula2/aula2_controle_fluxo.kt
@@ -2,7 +2,7 @@ package com.rafael.aula2
 
 fun main() {
 
-    val age = 18
+    var age = 18
 
     if (age >= 18) {
         println("Maior de idade")
@@ -32,4 +32,28 @@ fun main() {
         "Menor de idade"
     }
     println(message)
+
+    val day = 3
+
+    val dayName = when (day) {
+        1 -> "Domingo"
+        2 -> "Segunda"
+        3 -> "Terça"
+        4 -> "Quarta"
+        5 -> "Quinta"
+        6 -> "Sexta"
+        7 -> "Sábado"
+        else -> "Dia inválido"
+    }
+    println(dayName)   // Saída: Terça
+
+    age = 23
+
+    when (age) {
+        in 0..12 -> println("Criança")
+        in 13..17 -> println("Adolescente")
+        in 18..59 -> println("Adulto")
+        in 60..150 -> println("Idoso")
+        else -> println("Idade inválida")
+    }
 }
